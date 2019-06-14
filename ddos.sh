290092e903700046e1979eb076df6f83cc477aff
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-06-14 18:58:32 +0530

    Update ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-
+apt update
+apt upgrade
+apt list --upgradable
 apt install figlet
-apt inatall git -y
+apt install git -y
 apt install python -y
 pip install request
 pkg install ruby
@@ -9,31 +10,10 @@ pkg install php
 gem install lolcat
 git clone https://github.com/whitetigerz/DDOS-ATTACK
 figlet PRINCE YASIN |lolcat|lolcat
+#DONT MISSUED IT.
+#I AM NOT RESPONSIBLE FOR ANY ILLEGAL ACTIVITIES
 
 
-
-spin () {
-
-local pid=$!
-local delay=0.25
-local spinner=( '█■■■■' '■█■■■' '■■█■■' '■■■█■' '■■■■█' )
-
-while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-
-for i in "${spinner[@]}"
-do
-        tput civis
-        echo -ne "\033[34m\r[*] Downloading..please wait.........\e[3>
-        sleep $delay
-        printf "\b\b\b\b\b\b\b\b";
-done
-done
-printf "   \b\b\b\b\b"
-tput cnorm
-printf "\e[1;33m [Done]\e[0m";
-echo "";
-
-}
 
 
 
